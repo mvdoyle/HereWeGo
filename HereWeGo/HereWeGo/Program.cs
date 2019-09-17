0bc06361900e7e40f88ae7c48ab9f96506f3a82a
mvdoyle <mdoyle@truecoders.io>
2019-09-17 15:21:51 -0500

    first commit

---
+++ HereWeGo/HereWeGo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HereWeGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
