25ba81172232104821539e4ee0426bd4b9fa4f9a
mvdoyle <mdoyle@truecoders.io>
2020-03-30 13:09:50 -0500

    user question

--- HereWeGo/HereWeGo/Program.cs
+++ HereWeGo/HereWeGo/Program.cs
@@ -7,6 +7,7 @@ namespace HereWeGo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?"); 
         }
     }
 }
